e5afc95a97aec10da24d9de458118bc69b6c7fa1
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2025-04-03 17:59:59 +0530

    3-4-25

--- variables/p5.py
+++ variables/p5.py
@@ -1,11 +1,25 @@
-a=b=c=[0,1,2]
-[x,y,z]=[0,1,2]
-m=q=r=(1,2,3)
-u,t,s=(1,2,3)
+a=b=c=[0,1,2] #list
+
+[x,y,z]=[0.1,1,2] #list each x=0.1,y=1,z=2 
+
+m=q=r=(1,2,3) #tuple
+
+u,t,s=(1,2,3) #tuple  u=1 ,t=2,s=3
+
 e=i=o=(y*s)
-print(a,b,c)
+
+print(a,b,c) 
+
+print(x,y,z)
+
+print(m,q,r) 
+
 print('x:',type(x),x)
+
 print('a:',type(a),a)
+
 print('m:',type(m),m)
+
 print('u:',type(u),u)
+
 print('e:',type(e),e)
--- variables/p6.py
+++ variables/p6.py
@@ -1,4 +1,4 @@
-a=104022222222000000000000000000000000000
+a=1040222222220000000000000000000000000001
 b=10
 c=complex(4,5)
 d=12+5j+1.5j
@@ -11,3 +11,11 @@ print(b)
 print(c)
 print(d)
 
+utf8_bytes = 'स'.encode('utf-8')
+print(list(utf8_bytes)) 
+# a=float(input('give me a number'))
+# b=float(input('give me a number'))
+# # a=parseInt(prompt('give me a number'))#5
+# # b=parseInt(prompt('give me a number'))#6
+# c=a+b
+# print(c)
